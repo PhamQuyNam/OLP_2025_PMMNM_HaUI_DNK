54f7be00602eae0069ad1dd132c0b217f6321f52
PhamQuyNam <phamquynam2004@gmail.com>
2025-11-27 22:58:07 +0700

    update các service

--- report-service/src/index.js
+++ report-service/src/index.js
@@ -2,10 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const reportRoutes = require('./routes/reportRoutes');
+const swaggerSpecs = require('./config/swagger');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/api-docs.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpecs);
+});
 
 // Mount route
 app.use('/', reportRoutes);
--- weather-realtime-service/src/server.js
+++ weather-realtime-service/src/server.js
@@ -2,10 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const weatherRoutes = require('./routes/weatherRoutes');
+const swaggerSpecs = require('./config/swagger');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/api-docs.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpecs);
+});
 
 // Mount route
 // Gateway sẽ chuyển /api/weather -> vào đây
