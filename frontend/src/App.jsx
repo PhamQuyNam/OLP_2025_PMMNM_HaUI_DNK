82990b1e59969a4e96e492de506f9916cbf1a648
Gia Luat <luattrinh2k4@gmail.com>
2025-12-02 21:10:52 +0700

    fix: user page path error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing/LandingPage";
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
-
+import CitizenReportPage from "./pages/citizen/CitizenReportPage";
 // Citizen Imports
 import CitizenLayout from "./layouts/CitizenLayout";
 import CitizenHomePage from "./pages/citizen/CitizenHomePage";
@@ -19,9 +19,6 @@ import UnauthorizedPage from "./pages/auth/UnauthorizedPage";
 // Placeholder
 const CitizenAlerts = () => (
   <h1 className="text-center mt-20">Danh sách Cảnh báo (Coming Soon)</h1>
-);
-const CitizenReport = () => (
-  <h1 className="text-center mt-20">Gửi Báo cáo (Coming Soon)</h1>
 );
 
 function App() {
@@ -45,7 +42,7 @@ function App() {
           <Route path="/citizen" element={<CitizenLayout />}>
             <Route index element={<CitizenHomePage />} />
             <Route path="alerts" element={<CitizenAlerts />} />
-            <Route path="report" element={<CitizenReport />} />
+            <Route path="report" element={<CitizenReportPage />} />
             <Route path="profile" element={<CitizenProfile />} />
             <Route path="guide" element={<CitizenGuidePage />} />
           </Route>
--- frontend/src/services/reportService.js
+++ frontend/src/services/reportService.js
@@ -3,12 +3,12 @@ import axiosClient from "./axiosClient";
 const reportService = {
   // 1. Người dân gửi báo cáo (POST)
   createReport(data) {
-    return axiosClient.post("/reports", data);
+    return axiosClient.post("/reports/", data);
   },
 
   // 2. Quản lý lấy danh sách báo cáo (GET)
   getAllReports() {
-    return axiosClient.get("/reports");
+    return axiosClient.get("/reports/");
   },
 };
 
