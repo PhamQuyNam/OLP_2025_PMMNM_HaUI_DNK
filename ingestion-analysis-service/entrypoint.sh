20de5ab6dd3b660330df12cf3ae6582e4fb70fa8
NgoTan <ngovantannvt04@gmail.com>
2025-12-07 22:10:54 +0700

    fix socket

--- ingestion-analysis-service/entrypoint.sh
+++ ingestion-analysis-service/entrypoint.sh
@@ -35,14 +35,14 @@ echo "=== 2. CHẠY CÁC TÁC VỤ ETL (IMPORT DỮ LIỆU TĨNH TỰ ĐỘNG) =
 echo "========================================================="
 
 
-echo "➡️ B2: Chạy ETL: Import Waterways (Sông/Suối)"
-python -m etl.etl_import_waterways # <-- Dùng -m
+#echo "➡️ B2: Chạy ETL: Import Waterways (Sông/Suối)"
+#python -m etl.etl_import_waterways # <-- Dùng -m
 
-echo "➡️ B3: Chạy ETL: Import Stations (Trạm Quan Trắc)"
-python -m etl.etl_import_stations # <-- Dùng -m
+#echo "➡️ B3: Chạy ETL: Import Stations (Trạm Quan Trắc)"
+#python -m etl.etl_import_stations # <-- Dùng -m
 
-echo "➡️ B4: Chạy ETL: Profiling (Tính toán Slope, TWI, ISR...)"
-python -m etl.etl_station_profiling # <-- Dùng -m
+#echo "➡️ B4: Chạy ETL: Profiling (Tính toán Slope, TWI, ISR...)"
+#python -m etl.etl_station_profiling # <-- Dùng -m
 echo "✅ HOÀN TẤT ETL DỮ LIỆU TĨNH!"
 
 echo "========================================================="
